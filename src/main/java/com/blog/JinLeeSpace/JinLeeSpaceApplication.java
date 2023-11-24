3945167fa345cfff595dd09124e29fcaa17372bf
JinLeebriller <boomteru@naver.com>
2023-11-24 15:15:36 +0900

    test

---
+++ src/main/java/com/blog/JinLeeSpace/JinLeeSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.blog.JinLeeSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinLeeSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinLeeSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/JinLeeSpace/JinLeeSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.JinLeeSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinLeeSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
