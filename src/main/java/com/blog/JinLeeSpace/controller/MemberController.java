b0c80ac298c83d26ae6e52185b7a9fe0dd4c70b3
JinLeebriller <boomteru@naver.com>
2023-11-24 20:10:58 +0900

    test

--- src/main/java/com/blog/JinLeeSpace/controller/MemberController.java
+++ src/main/java/com/blog/JinLeeSpace/controller/MemberController.java
@@ -1,4 +1,26 @@
 package com.blog.JinLeeSpace.controller;
 
+import com.blog.JinLeeSpace.dto.MemberFormDto;
+import com.blog.JinLeeSpace.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    // 회원가입 페이지로 이동
+    @GetMapping("/new")
+    public String memberForm(Model model) {
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        return "member/memberForm";
+    }
+
 }
