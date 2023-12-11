f293fa5b0acae3f6043481df6691b43b63c4245d
JinLeebriller <boomteru@naver.com>
2023-12-11 21:30:40 +0900

    test

--- src/main/java/com/blog/JinLeeSpace/controller/PostController.java
+++ src/main/java/com/blog/JinLeeSpace/controller/PostController.java
@@ -2,12 +2,14 @@ package com.blog.JinLeeSpace.controller;
 
 import com.blog.JinLeeSpace.dto.PostFormDto;
 import com.blog.JinLeeSpace.service.PostService;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -52,6 +54,22 @@ public class PostController {
         }
 
         return "redirect:/";
+    }
+
+    // 포스트 수정 페이지로 이동
+    @GetMapping("/post/{idNumber}")
+    public String postDtl(@PathVariable("idNumber") Long idNumber, Model model) {
+
+        try {
+            PostFormDto postFormDto = postService.getPostDtl(idNumber);
+            model.addAttribute("postFormDto", postFormDto);
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("errorMessage", "존재하지 않는 포스트입니다.");
+            model.addAttribute("postFormDto", new PostFormDto());
+            return "post/postForm";
+        }
+
+        return "post/postForm";
     }
 
 }
