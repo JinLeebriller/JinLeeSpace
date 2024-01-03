5c42bf9cd9f2be14ccc9d68880b31589c6f2ce2b
JinLeebriller <boomteru@naver.com>
2024-01-03 14:17:03 +0900

    test

--- src/main/java/com/blog/JinLeeSpace/controller/MainController.java
+++ src/main/java/com/blog/JinLeeSpace/controller/MainController.java
@@ -10,6 +10,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.Optional;
 
 @Controller
@@ -19,7 +21,7 @@ public class MainController {
     private final PostService postService;
 
     @GetMapping("/")
-    public String main(PostSearchDto postSearchDto, Optional<Integer> page, Model model) {
+    public String main(PostSearchDto postSearchDto, @PathVariable("page") Optional<Integer> page, Model model) {
         Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 3);
         Page<MainPostDto> postList = postService.getMainPostPage(postSearchDto, pageable);
         model.addAttribute("postList", postList);
