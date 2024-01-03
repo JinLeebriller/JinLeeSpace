f1fa34437846b6033b42c71a6e02a0a3da08026b
JinLeebriller <boomteru@naver.com>
2024-01-03 13:52:09 +0900

    test

--- src/main/java/com/blog/JinLeeSpace/controller/MainController.java
+++ src/main/java/com/blog/JinLeeSpace/controller/MainController.java
@@ -1,13 +1,30 @@
 package com.blog.JinLeeSpace.controller;
 
+import com.blog.JinLeeSpace.dto.MainPostDto;
+import com.blog.JinLeeSpace.dto.PostSearchDto;
+import com.blog.JinLeeSpace.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Optional;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 
+    private final PostService postService;
+
     @GetMapping("/")
-    public String main(){
+    public String main(PostSearchDto postSearchDto, Optional<Integer> page, Model model) {
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 3);
+        Page<MainPostDto> postList = postService.getMainPostPage(postSearchDto, pageable);
+        model.addAttribute("postList", postList);
+        model.addAttribute("postSearchDto", postSearchDto);
+        model.addAttribute("maxPage", 5);
         return "main";
     }
 
